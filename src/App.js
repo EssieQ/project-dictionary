2b4423cf336bffd69df2fa763613d6e0fe2fe9ab
EssieQ <ecauven@gmail.com>
2021-08-18 17:43:38 +1000

    Added search form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary App</header>
+      <div className="container">
+        <header className="App-header">Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Esther Cauven</footer>
+      </div>
     </div>
   );
 }
