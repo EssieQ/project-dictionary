307fac0f430d365fc2a5c908c1e92d84dcd1bbda
EssieQ <ecauven@gmail.com>
2021-08-25 19:46:40 +1000

    Styled the application

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,19 +1,17 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
-
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
-            <br />
-            <em>
-              <p>{definition.example}</p>
-            </em>
+            <div className="definition">{definition.definition}</div>
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
