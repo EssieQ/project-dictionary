1b282056f76d0e4eb902f375e9e7d637c3d18878
EssieQ <ecauven@gmail.com>
2021-08-31 17:21:50 +1000

    Added my own logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import "./App.css";
+import logo from "./logo.png";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Dictionary App</header>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
-        <footer className="App-footer">Coded by Esther Cauven</footer>
+        <footer className="App-footer">
+          This dictionary is coded by {""}
+          <a
+            href="https://www.linkedin.com/in/esthercauven/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Esther Cauven
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/EssieQ/project-dictionary"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
       </div>
     </div>
   );
